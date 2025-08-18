6c87b5a31b17c1db0f48d6beb557d01fb6138e65
TenorioJanssen <2220552@ub.edu.ph>
2025-08-18 10:15:12 +0800

    Update Code for August 18 2025
    Activity#1 Debounce

---
+++ Activity #1/Debounce/Debounce.ino
@@ -0,0 +1,44 @@
+const int buttonPin = 15;  
+const int ledPin = 2;    
+
+
+int ledState = HIGH;      
+int buttonState;           
+int lastButtonState = LOW;  
+
+unsigned long lastDebounceTime = 0;  
+unsigned long debounceDelay = 50;   
+
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+  digitalWrite(ledPin, ledState);
+}
+
+void loop() {
+ 
+  int reading = digitalRead(buttonPin);
+
+
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+      }
+    }
+  }
+
+
+  digitalWrite(ledPin, ledState);
+
+  lastButtonState = reading;
+}
