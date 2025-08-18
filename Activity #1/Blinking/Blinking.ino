63cb8acbae4fb529488169a73c7beb2cf21cd46f
Athelstan2727 <2220677@ub.edu.ph>
2025-08-18 10:45:50 +0800

    August 18, 2025
    Blinking activity

--- Activity #1/Blinking/Blinking.ino
+++ Activity #1/Blinking/Blinking.ino
@@ -1,5 +1,5 @@
 
-// the setup function runs once when you press reset or power the board
+// a
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
