53e795fbc81247dd3dbc09e629dff55688b7dbc6
Queenie Marie Magcawas <2321197@ub.edu.ph>
2025-08-18 10:55:18 +0800

    Update code for Debounce, August 18, 2025
    Activity 1 Debounce

--- Activity #1/Debounce/Debounce.ino
+++ Activity #1/Debounce/Debounce.ino
@@ -1,44 +1,43 @@
-const int buttonPin = 12;  
-const int ledPin = 2;    
+const int buttonPin = 32;    // Push button pin
+const int ledPin = 2;        // LED pin (Onboard LED on many ESP32s)
 
+// Debounce variables
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;  // milliseconds
 
-int ledState = HIGH;      
-int buttonState;           
-int lastButtonState = LOW;  
-
-unsigned long lastDebounceTime = 0;  
-unsigned long debounceDelay = 50;   
-
+int lastButtonState = HIGH;    // Assume button is not pressed initially
+int buttonState;
+int ledState = LOW;
 
 void setup() {
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP); // Use internal pull-up
   pinMode(ledPin, OUTPUT);
-
   digitalWrite(ledPin, ledState);
+
+  Serial.begin(115200);
 }
 
 void loop() {
- 
   int reading = digitalRead(buttonPin);
 
-
   if (reading != lastButtonState) {
+    // reset debounce timer
     lastDebounceTime = millis();
   }
 
   if ((millis() - lastDebounceTime) > debounceDelay) {
-
+    // if the button state has stabilized
     if (reading != buttonState) {
       buttonState = reading;
 
-      if (buttonState == HIGH) {
+      // only toggle the LED if the new state is LOW (button press)
+      if (buttonState == LOW) {
         ledState = !ledState;
+        digitalWrite(ledPin, ledState);
+        Serial.println("Button Pressed - LED Toggled");
       }
     }
   }
-
-
-  digitalWrite(ledPin, ledState);
 
   lastButtonState = reading;
 }
