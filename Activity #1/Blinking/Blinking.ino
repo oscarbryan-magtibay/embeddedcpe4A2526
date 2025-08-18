98fd8a367d4acb3d96f6e31673bde63ea0f63d18
Jhon Paul Ojales <2220478@ub.edu.ph>
2025-08-18 13:12:58 +0800

    Updated Code for August 18 2025
    Activity#1-Blinking

--- Activity #1/Blinking/Blinking.ino
+++ Activity #1/Blinking/Blinking.ino
@@ -1,12 +1,12 @@
-#define LED_PIN 2   
+#define LED_PIN 2   // Onboard LED is usually on GPIO 2 for ESP32
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT); 
+  pinMode(LED_PIN, OUTPUT); // Set LED pin as output
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH); 
-  delay(1000);                
-  digitalWrite(LED_PIN, LOW); 
-  delay(1000);                 
+  digitalWrite(LED_PIN, HIGH); // Turn LED on
+  delay(1000);                 // Wait 1 second
+  digitalWrite(LED_PIN, LOW);  // Turn LED off
+  delay(1000);                 // Wait 1 second
 }
