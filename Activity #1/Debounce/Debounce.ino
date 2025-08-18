66dac40c396a7670eea83a6f49ed213f3d6fe8bf
hajas0708 <1620450@ub.edu.ph>
2025-08-18 12:56:37 +0800

    Activity 1 - Embedded

---
+++ Activity #1/Debounce/Debounce.ino
@@ -0,0 +1,37 @@
+const int led = 2;
+const int button = 4;
+
+int ledState = HIGH;
+int buttonState;
+int lastButtonState = LOW;
+
+unsigned long lastDebounceTime = 0; 
+unsigned long debounceDelay = 50;
+
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT_PULLUP);
+  digitalWrite(led, ledState);
+}
+
+void loop() {
+  int reading = digitalRead(button);
+
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+      }
+    }
+  }
+  digitalWrite(led, ledState);
+  lastButtonState = reading;
+}
