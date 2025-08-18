59da5e789d88129ccf600db6a3e32acd09799561
AtillanoMarkBeaver <2220433@ub.edu.ph>
2025-08-18 11:53:03 +0800

    debounce updated code 8/18/2025

---
+++ Activity #1/Debounce/Debounce.ino
@@ -0,0 +1,48 @@
+const int buttonPin = 0;  
+const int ledPin = 2;     
+
+// a
+int ledState = HIGH;        
+int buttonState;            
+int lastButtonState = LOW;  
+
+
+unsigned long lastDebounceTime = 0;  
+unsigned long debounceDelay = 50;    
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);  
+  pinMode(ledPin, OUTPUT);
+
+  
+  digitalWrite(ledPin, ledState);
+}
+
+void loop() {
+ 
+  int reading = digitalRead(buttonPin);
+
+ 
+  if (reading != lastButtonState) {
+   
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      
+      if (buttonState == LOW) {
+        ledState = !ledState; 
+      }
+    }
+  }
+
+
+  digitalWrite(ledPin, ledState);
+
+  
+  lastButtonState = reading;
+}
