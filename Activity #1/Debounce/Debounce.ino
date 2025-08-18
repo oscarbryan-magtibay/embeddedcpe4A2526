c12df0364d388aaf2a6a64a1a3b576d243a1d63e
Jhon Paul Ojales <2220478@ub.edu.ph>
2025-08-18 11:53:46 +0800

    Updated Code for Debounce August 18 2025
    Activity-1_Debounce

--- Activity #1/Debounce/Debounce.ino
+++ Activity #1/Debounce/Debounce.ino
@@ -1,44 +1,38 @@
-const int buttonPin = 10;  
-const int ledPin = 3;    
+const int buttonPin = 14;    
+const int ledPin = 2;        
 
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;  
 
-int ledState = HIGH;      
-int buttonState;           
-int lastButtonState = LOW;  
-
-unsigned long lastDebounceTime = 0;  
-unsigned long debounceDelay = 50;   
-
+int lastButtonState = HIGH;   
+int buttonState;
+int ledState = LOW;
 
 void setup() {
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP); 
   pinMode(ledPin, OUTPUT);
-
   digitalWrite(ledPin, ledState);
+
+  Serial.begin(115200);
 }
 
 void loop() {
- 
   int reading = digitalRead(buttonPin);
-
 
   if (reading != lastButtonState) {
     lastDebounceTime = millis();
   }
 
   if ((millis() - lastDebounceTime) > debounceDelay) {
-
     if (reading != buttonState) {
       buttonState = reading;
-
-      if (buttonState == HIGH) {
+      if (buttonState == LOW) {
         ledState = !ledState;
+        digitalWrite(ledPin, ledState);
+        Serial.println("Button Pressed - LED Toggled");
       }
     }
   }
-
-
-  digitalWrite(ledPin, ledState);
 
   lastButtonState = reading;
 }
