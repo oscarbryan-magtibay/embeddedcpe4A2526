cbdbd61714f82006583a86bd7fc696a872fefd5e
Queenie Marie Magcawas <2321197@ub.edu.ph>
2025-08-18 12:26:00 +0800

    Update Code for Servo Sweep August 18, 2025
    Activity 1 Servo Sweep

---
+++ Activity #1/ServoSweep/ServoSweep.ino
@@ -0,0 +1,31 @@
+#include <ESP32Servo.h> 
+const int potPin = 34;  
+const int servoPin = 12; 
+
+Servo myServo; 
+
+void setup() {
+  
+  myServo.attach(servoPin);
+  
+  
+  Serial.begin(115200);
+}
+
+void loop() {
+  int potValue = analogRead(potPin);
+
+  
+  int servoAngle = map(potValue, 0, 4095, 0, 180);
+
+  
+  myServo.write(servoAngle);
+  
+ 
+  Serial.print("Potentiometer Value: ");
+  Serial.print(potValue);
+  Serial.print(" | Servo Angle: ");
+  Serial.println(servoAngle);
+
+  delay(15); 
+}
