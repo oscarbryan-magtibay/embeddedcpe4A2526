83d51d256b903138257fb9abe6d0897044f51728
Queenie Marie Magcawas <2321197@ub.edu.ph>
2025-08-18 11:53:19 +0800

    Updated Code for Dimming August 18, 2025
    Activity 1 Dimmming

---
+++ Activity #1/Dimming/Dimming.ino
@@ -0,0 +1,29 @@
+const int ledPin = 2;      
+const int potPin = 34;     
+int potValue = 0;         
+int pwmValue = 0;          
+
+
+void setup() {
+
+  pinMode(ledPin, OUTPUT);
+
+  Serial.begin(115200);
+
+}
+
+
+void loop() {
+  potValue = analogRead(potPin);
+  pwmValue = map(potValue, 0, 4095, 0, 255);
+
+   analogWrite(ledPin, pwmValue);
+
+  Serial.print("Potentiometer Value: ");
+  Serial.print(potValue);
+  Serial.print(" | PWM Value: ");
+  Serial.println(pwmValue);
+
+  delay(10);
+
+}
