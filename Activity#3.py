2e170ef2b15f74467141184d87de02a4b92668b0
Dale Vincent Dimaano <2020435@ub.edu.ph>
2025-09-10 15:39:40 +0800

    September 10,2025
    Activity 3

---
+++ Activity#3.py
@@ -0,0 +1,23 @@
+#include <ESP32Servo.h>
+
+Servo myServo;  
+int angle;
+
+void setup() {
+  Serial.begin(115200);  
+  myServo.attach(13);
+  Serial.println("Enter angle: ");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    angle = Serial.parseInt();
+    if (angle >= 0 && angle <= 180) {
+      myServo.write(angle);  
+      Serial.print("Servo moved to: ");
+      Serial.println(angle);
+    } else {
+      Serial.println("Enter 0 to 180 only.");
+    }
+  }
+}
