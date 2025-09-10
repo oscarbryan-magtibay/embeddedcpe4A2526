898aff3d2ed30e57a9b5fdabb8adab62a26666ee
Queenie Marie Magcawas <2321197@ub.edu.ph>
2025-09-10 15:59:36 +0800

    Updated Code for September 10, 2025
    Activity 2

--- Activity 2/Dimming/Dimming.ino
+++ Activity 2/Dimming/Dimming.ino
@@ -1,29 +1,35 @@
-const int ledPin = 2;      
-const int potPin = 34;     
-int potValue = 0;         
-int pwmValue = 0;          
-
+const int ledPin = 2;      // LED pin
+const int potPin = 34;     // Potentiometer pin
+int potValue = 0;          // Variable to store potentiometer value
+int dutyCycle = 0;        
 
 void setup() {
-
   pinMode(ledPin, OUTPUT);
-
   Serial.begin(115200);
-
 }
 
-
 void loop() {
-  potValue = analogRead(potPin);
-  pwmValue = map(potValue, 0, 4095, 0, 255);
 
-   analogWrite(ledPin, pwmValue);
+  potValue = analogRead(potPin);
+  dutyCycle = map(potValue, 0, 4095, 0, 100);
+
+  int period = 1000;
+  int onTime = (dutyCycle * period) / 100;
+  int offTime = period - onTime;
+
+  if (onTime > 0) {
+    digitalWrite(ledPin, HIGH);
+    delayMicroseconds(onTime);
+  }
+  if (offTime > 0) {
+    digitalWrite(ledPin, LOW);
+    delayMicroseconds(offTime);
+  }
+
 
   Serial.print("Potentiometer Value: ");
   Serial.print(potValue);
-  Serial.print(" | PWM Value: ");
-  Serial.println(pwmValue);
-
-  delay(10);
-
+  Serial.print(" | Duty Cycle: ");
+  Serial.print(dutyCycle);
+  Serial.println("%");
 }
