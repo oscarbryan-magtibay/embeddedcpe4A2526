6573cb0357c1cb50e502ec919af3009bd3bd73b5
Queenie Marie Magcawas <2321197@ub.edu.ph>
2025-09-10 11:47:26 +0800

    Updated Code for September 10, 2025
    Activity 3

---
+++ Activity 3/servo/servo.ino
@@ -0,0 +1,29 @@
+#include <ESP32Servo.h>
+
+Servo myServo;
+int servoPin = 25;  
+
+void setup() {
+  Serial.begin(115200);
+  myServo.attach(servoPin);
+  myServo.write(0); 
+  Serial.println("Type any angle (0-180) in Serial Monitor to move servo.");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    input.trim();  
+
+    int angle = input.toInt(); 
+
+    if (angle >= 0 && angle <= 180) {
+      myServo.write(angle);
+      Serial.print("Servo moved to ");
+      Serial.print(angle);
+      Serial.println("°");
+    } else {
+      Serial.println("Error: Please enter a value between 0 and 180.");
+    }
+  }
+}
